53a0fc7c6c54a49d9b8f1d684997b3bd926b0023
DINight123 <gimein24@gmail.com>
2026-06-24 22:28:47 +0300

    Update run_windows.sh

--- run_windows.sh
+++ run_windows.sh
@@ -1,7 +1,7 @@
+cd yatube
 python -m venv .venv
 . .venv/Scripts/activate
+cd yatubeeri
 pip install -r requirements.txt
-cd my_site
+python manage.py migrate
 python manage.py runserver
-
-Добавить миграцию
